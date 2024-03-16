2aac97cfc3c8ae5339bea0d936eb589150c6a5a8
Daniel Milchev <35624892+Daniel10106@users.noreply.github.com>
2024-03-16 14:08:11 +0200

    Restored Comment class

---
+++ TheoryProtocol.Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using Google.Cloud.Firestore;
+
+namespace TheoryProtocol.Models
+{
+    [FirestoreData]
+    public class Comment
+    {
+        [FirestoreProperty] public int Id { get; set; }
+        [FirestoreProperty] public int UserId { get; set; }
+        [FirestoreProperty] public int ConnetionId { get; set; }
+        [FirestoreProperty] public string? Content { get; set; }
+    }
+}
