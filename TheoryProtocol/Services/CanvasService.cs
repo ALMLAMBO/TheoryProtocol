9fce84da6039d3a6d81f3d182d57c0b6d4401eb4
tegu1365 <tedi.d.petkova@gmail.com>
2024-03-16 21:59:35 +0200

    Canvas menu by canvases in db

---
+++ TheoryProtocol/Services/CanvasService.cs
@@ -0,0 +1,20 @@
+﻿using TheoryProtocol.Models;
+using TheoryProtocol.Repositories;
+
+namespace TheoryProtocol.Services
+{
+    public class CanvasService
+    {
+        private readonly CanvasRepository _repository;
+        public CanvasService(CanvasRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<Models.Canvas>> GetCanvas()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+    }
+}
