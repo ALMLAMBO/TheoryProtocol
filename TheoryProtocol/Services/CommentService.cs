7afa51d72fb01651c5654cec9f09c99cc4208831
Daniel Milchev <35624892+Daniel10106@users.noreply.github.com>
2024-03-17 06:05:21 +0200

    Added CommentService

---
+++ TheoryProtocol/Services/CommentService.cs
@@ -0,0 +1,28 @@
+﻿using TheoryProtocol.Models;
+using TheoryProtocol.Repositories;
+
+namespace TheoryProtocol.Services
+{
+    public class CommentService
+    {
+        private readonly CommentRepository _repository;
+        private string _idDocumentName = "commentsId";
+        public CommentService(CommentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<Comment>> GetCommentsByCanvasAsync(int canvasId)
+        {
+            return await _repository.GetByFieldIdAsync("CanvasId",canvasId);
+        }
+
+        public async void AddComment(Comment comment)
+        {
+            int prevId = await _repository.GetIdAsync(_idDocumentName);
+            comment.Id = prevId + 1;
+            await _repository.UpdateIdAsync(_idDocumentName);
+            await _repository.AddAsync(comment);
+        }
+    }
+}
