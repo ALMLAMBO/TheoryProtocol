7319668ef7941fbbde9908eaacd7c4220bf3121b
slavi22 <slavi228@gmail.com>
2024-03-17 02:33:06 +0200

    Finished the create canvas component

---
+++ TheoryProtocol/Models/CreateCanvasModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheoryProtocol.Models;
+
+public class CreateCanvasModel
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+}
